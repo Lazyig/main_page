e2608330d9c177ddda9955724e7133230a7d53f9
lazyig <kimig009@gmail.com>
2019-06-21 08:22:43 +0900

    404 fix

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const app = express();
 
 const PORT = 30000;
 
-app.use((req, res, next) => {
-    fs.readFile('404.html', (err, data) => {
-        return res.status(404).end(data, 404);
-    });
-});
-
 app.get('/', (req, res) => {
     fs.readFile('index.html', (err, data) => {
         res.end(data);
     });
 });
 
+app.use((req, res, next) => {
+    fs.readFile('404.html', (err, data) => {
+        return res.status(404).end(data, 404);
+    });
+});
 
 app.listen(PORT, (err) => {
     console.log('Main Page Server On');
