d1ca32cc55391511c1bef12156a099c697981a77
lazyig <kimig009@gmail.com>
2019-06-21 08:13:50 +0900

    fix

--- index.js
+++ index.js
@@ -6,16 +6,10 @@ const app = express();
 
 const PORT = 30000;
 
-<<<<<<< HEAD
 app.use((req, res) => {
     res.status(404);
+    
     fs.readFile('404.html', (err, data) => {
-=======
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-    fs.readFile('index.html', (err, data) => {
->>>>>>> 9c55e4b1d8c661813dac52171d4778e913b8d422
         res.end(data);
     });
 });
