1b65d4e0a522997af9e58167c1f810690366eaeb
lazyig <kimig009@gmail.com>
2019-10-22 19:18:58 +0900

    정적 파일 로드 코드 추가

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 30000;
 
-app.get('/', (req, res) => {
-    fs.readFile('index.html', (err, data) => {
-        res.end(data);
-    });
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+  fs.readFile("index.html", (err, data) => {
+    res.end(data);
+  });
 });
 
 app.use((req, res, next) => {
-    fs.readFile('404.html', (err, data) => {
-        return res.status(404).end(data, 404);
-    });
+  fs.readFile("404.html", (err, data) => {
+    return res.status(404).end(data, 404);
+  });
 });
 
-app.listen(PORT, (err) => {
-    console.log('Main Page Server On');
-    console.log('Port : ' + PORT);
+app.listen(PORT, err => {
+  console.log("Main Page Server On");
+  console.log("Port : " + PORT);
 });
